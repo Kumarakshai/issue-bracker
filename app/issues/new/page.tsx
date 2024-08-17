0390bc894ecb87cf97bc0d79abc6370dfde406b8
AkshaiKumar <kumarakshai2425@gmail.com>
2024-08-17 11:18:49 +0530

    add markdoen editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,13 +1,15 @@
 "use client";
 import { Button, TextArea, TextField } from "@radix-ui/themes";
 import React from "react";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 
 const NewIssuesPage = () => {
   return (
     <div className="max-w-xl space-y-3">
       {/* <TextField.Root> */}
       <TextField.Root placeholder="Title" />
-      <TextArea size="3" placeholder="Description" />
+      <SimpleMDE placeholder="Description" />
       <Button>Submit New Issue</Button>
 
       {/* </TextField.Root> */}
